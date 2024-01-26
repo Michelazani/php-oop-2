ff9ee94419824cb8fc5fbf54a0b01f82ad806694
Michelazani <michi.zani220@gmail.com>
2024-01-26 16:08:53 +0100

    divisione file

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+require_once __DIR__ . '/models/AnimalProduct.php';
+require_once __DIR__ . '/models/Dog.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Animal Product</title>
+</head>
+<body>
+    
+</body>
+</html>
---
+++ models/Dog.php
@@ -0,0 +1,7 @@
+<?php
+require_once __DIR__ . '/models/AnimalProduct.php';
+
+    class Dog extends AnimalProduct{
+        
+    }
+?>
